46323338412b8c7d14b36f467c4766a672e9c770
cosmos-fly <coder211@163.com>
2021-07-07 09:06:56 +0800

    示例1：演示最简单的变量

--- ConsoleApp01/Lesson01.cs
+++ ConsoleApp01/Lesson01.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConsoleApp01
+﻿namespace ConsoleApp01
 {
     internal class Lesson01
     {
+        public void eg01()
+        {
+            // 示例1：演示最简单的变量
+            var n = 100;
+            var m = 200;
+            var sum = 0;
+            sum = n + m;
+            string prefix = "计算结果是：";
+            Console.WriteLine(prefix);
+            Console.WriteLine(sum);
+            // 接收用户输入一个键
+            Console.ReadKey();
+        }
     }
 }
--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ConsoleApp01;
+
+class Program
+{
+    static void Main()
+    {
+        Lesson01 lesson01 = new Lesson01();
+        lesson01.eg01();
+        Console.ReadKey();
+    }
+}
