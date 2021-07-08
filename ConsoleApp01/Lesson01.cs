1b37f860e706ccf2ed508cfeeb5b683d62afeba9
cosmos-fly <coder211@163.com>
2021-07-08 10:14:27 +0800

    示例2：类型转换和预设值

--- ConsoleApp01/Lesson01.cs
+++ ConsoleApp01/Lesson01.cs
@@ -15,5 +15,18 @@
             // 接收用户输入一个键
             Console.ReadKey();
         }
+
+        public void eg02()
+        {
+            // 示例2：类型转换和预设值
+            Console.WriteLine("请输入第一个数字：");
+            string s1 = Console.ReadLine() ?? "";
+            Console.WriteLine("请输入第二个数字：");
+            string s2 = Console.ReadLine() ?? "";
+            string str = s1 + s2;
+            Console.WriteLine("字符拼接：" + str);
+            int value = int.Parse(s1) + int.Parse(s2);
+            Console.WriteLine("数字相加：" + value.ToString());
+        }
     }
 }
--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         Lesson01 lesson01 = new Lesson01();
-        lesson01.eg01();
+        lesson01.eg02();
         Console.ReadKey();
     }
 }
