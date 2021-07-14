7aa592f9fcfc1c1fa3649fa51ff14ed024185f96
cosmos-fly <coder211@163.com>
2021-07-14 09:19:53 +0800

    示例8：添加类库项目

---
+++ ConsoleApp01-MyLib/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp01_MyLib
+{
+    // 示例8所使用的类
+    public class Student
+    {
+        public string name { get; set; }
+        public uint age { get; set; }
+        public string grade { get; set; }
+
+        public Student(string name, uint age, string grade)
+        {
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+        }
+
+        public string getInfo()
+        {
+            return $"Student[{name}, {age}, {grade}]";
+        }
+    }
+}
