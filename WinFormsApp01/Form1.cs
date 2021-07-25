ac5121a5ef3d40e5a9ba182fdca8f88bd774205e
cosmos-fly <coder211@163.com>
2021-07-25 09:29:43 +0800

    WF08：撤销前两次修改，即撤销WF07和WF06

--- WinFormsApp01/Form1.cs
+++ WinFormsApp01/Form1.cs
@@ -5,13 +5,6 @@ namespace WinFormsApp01
         public Form1()
         {
             InitializeComponent();
-            // 在窗体中手动添加一个按钮
-            Button btn = new Button();
-            btn.Text = "一个按钮";
-            btn.Left = 20;
-            btn.Top = 20;
-            btn.Size = new Size(150, 50);
-            this.Controls.Add(btn);
         }
     }
 }
