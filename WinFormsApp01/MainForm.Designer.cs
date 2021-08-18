26129244eeb57d6e265b895192838ceccbd7a544
cosmos-fly <coder211@163.com>
2021-08-18 09:02:39 +0800

    WF30: 添加标签和文本框作为计算器的输入区域

--- WinFormsApp01/MainForm.Designer.cs
+++ WinFormsApp01/MainForm.Designer.cs
@@ -30,6 +30,12 @@ namespace WinFormsApp01
         {
             labelWelcome = new Label();
             btnCaculate = new Button();
+            labelPrice = new Label();
+            textPrice = new TextBox();
+            textNumber = new TextBox();
+            labelNumber = new Label();
+            textTotal = new TextBox();
+            labelTotal = new Label();
             SuspendLayout();
             // 
             // labelWelcome
@@ -50,11 +56,70 @@ namespace WinFormsApp01
             btnCaculate.Text = "计算总价";
             btnCaculate.UseVisualStyleBackColor = true;
             // 
+            // labelPrice
+            // 
+            labelPrice.AutoSize = true;
+            labelPrice.Location = new Point(253, 133);
+            labelPrice.Name = "labelPrice";
+            labelPrice.Size = new Size(62, 31);
+            labelPrice.TabIndex = 2;
+            labelPrice.Text = "单价";
+            // 
+            // textPrice
+            // 
+            textPrice.Location = new Point(359, 133);
+            textPrice.MaxLength = 8;
+            textPrice.Name = "textPrice";
+            textPrice.Size = new Size(253, 38);
+            textPrice.TabIndex = 3;
+            // 
+            // textNumber
+            // 
+            textNumber.Location = new Point(359, 187);
+            textNumber.MaxLength = 8;
+            textNumber.Name = "textNumber";
+            textNumber.Size = new Size(253, 38);
+            textNumber.TabIndex = 5;
+            // 
+            // labelNumber
+            // 
+            labelNumber.AutoSize = true;
+            labelNumber.Location = new Point(253, 187);
+            labelNumber.Name = "labelNumber";
+            labelNumber.Size = new Size(62, 31);
+            labelNumber.TabIndex = 4;
+            labelNumber.Text = "数量";
+            // 
+            // textTotal
+            // 
+            textTotal.BackColor = SystemColors.ButtonHighlight;
+            textTotal.Location = new Point(359, 240);
+            textTotal.MaxLength = 17;
+            textTotal.Name = "textTotal";
+            textTotal.ReadOnly = true;
+            textTotal.Size = new Size(253, 38);
+            textTotal.TabIndex = 7;
+            // 
+            // labelTotal
+            // 
+            labelTotal.AutoSize = true;
+            labelTotal.Location = new Point(253, 240);
+            labelTotal.Name = "labelTotal";
+            labelTotal.Size = new Size(62, 31);
+            labelTotal.TabIndex = 6;
+            labelTotal.Text = "总价";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(14F, 31F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textTotal);
+            Controls.Add(labelTotal);
+            Controls.Add(textNumber);
+            Controls.Add(labelNumber);
+            Controls.Add(textPrice);
+            Controls.Add(labelPrice);
             Controls.Add(btnCaculate);
             Controls.Add(labelWelcome);
             Name = "MainForm";
@@ -69,5 +134,11 @@ namespace WinFormsApp01
 
         private Label labelWelcome;
         private Button btnCaculate;
+        private Label labelPrice;
+        private TextBox textPrice;
+        private TextBox textNumber;
+        private Label labelNumber;
+        private TextBox textTotal;
+        private Label labelTotal;
     }
 }
